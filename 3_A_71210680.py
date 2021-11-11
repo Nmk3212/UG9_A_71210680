3639ea8dc633d49d7228d40d642baf2ee13ab44e
Marcell <nicholasmarcell2003@gmail.com>
2021-11-11 16:10:37 +0700

    Tambah file 3_A_71210680

---
+++ 3_A_71210680.py
@@ -0,0 +1,10 @@
+d1 = input("Masukan daftar belanja 1 : ")
+d2 = input("Masukan daftar belanja 2 : ")
+tambah_d1 = input("Tambahkan data ke daftar belanja 1 : ")
+tambah_d2 = input("Tambahkan data ke daftar belanja 2 : ")
+
+d_belanja1 = [d1, tambah_d1]
+d_belanja2 = [d2, tambah_d2]
+
+print("Daftar belanja 1 adalah", d_b1[0], d_b1[1])
+print("Daftar belanja 2 adalah", d_b2[0], d_b2[1])
